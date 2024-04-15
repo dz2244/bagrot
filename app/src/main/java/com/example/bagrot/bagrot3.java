21edb3e8ff4265b99ce2568b6bfb8b05f642b05f
dz2244 <dz2244@bs.amalnet.k12.il>
2024-04-15 03:00:05 +0300

    start 3 xml

---
+++ app/src/main/java/com/example/bagrot/bagrot3.java
@@ -0,0 +1,14 @@
+package com.example.bagrot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bagrot3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bagrot3);
+    }
+}
